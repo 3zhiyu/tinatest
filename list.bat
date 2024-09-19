6333e6fce94b28174cff4457f991262cebfc9faf
tina458615 <dog458615@hotmail.com>
2024-09-19 16:25:47 +0800

    測試登入功能

--- list.bat
+++ list.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 set "list="
 
-for /r %%i in (*.jpg, *.jpeg, *.png, *.gif, *.bmp, *.tif, *.tiff) do (
+for /r %%i in (*.jpg, *.jpeg, *.png, *.gif, *.bmp, *.tif, *.tiff, *.svg) do (
     if "%%~nxi" neq "list.bat" (
         set "filename=%%~nxi"
         set "list=!list! 'media/!filename!'"
